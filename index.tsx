8bb43471fbe0833d0c9b151c69f2e44c0314aa37
Aurfex <Bagheri.dariush@gmail.com>
2026-02-25 23:27:25 -0500

    Migrate Tailwind CDN to production PostCSS build

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './contexts/AppContext';
 import { LanguageProvider } from './contexts/LanguageContext';
-
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
